c9c8c308e3761694bf1ee88ccff71bed0ac2bc83
Matjaž Domen Pečan <matjaz.pecan@affinitas.de>
2019-05-17 11:39:59 +0200

    Move to let instead of using a mutable list.

--- src/main/kotlin/si/pecan/service/CostRetrievalService.kt
+++ src/main/kotlin/si/pecan/service/CostRetrievalService.kt
@@ -25,38 +25,44 @@ class CostRetrievalService(private val costRecordsRepository: CostRecordsReposit
 }
 
 
+// Using deprecated method as the static function Specification.where does not have the correct @Nullable annotation on its parameter value
+@Suppress("DEPRECATION")
 fun CostFilter?.toSpecification(): Specification<CostRecord> {
-    val specifications = mutableListOf<Specification<CostRecord>>()
-    // Using deprecated method as the static function Specification.where does not have the correct @Nullable annotation on its parameter value
-    @Suppress("DEPRECATION")
-    specifications.add(Specifications.where<CostRecord>(null))
-    if (this?.discharges != null) {
-        val (min, max) = this.discharges
-        rangeSpecification(min, max, specifications, CostRecord_.totalDischarges)
-    }
-    if (this?.averageCoveredCharges != null) {
-        val (min, max) = this.averageCoveredCharges
-        rangeSpecification(min, max, specifications, CostRecord_.averageCoveredCharges)
-    }
-    if (this?.averageMedicarePayments != null) {
-        val (min, max) = this.averageMedicarePayments
-        rangeSpecification(min, max, specifications, CostRecord_.averageMedicarePayments)
-    }
-    if (this?.state != null) {
-        specifications.add(Specification { record, _, criteriaBuilder ->
+    this ?: return Specifications.where<CostRecord>(null)
+    return Specifications.where<CostRecord>(null).let {
+        if (this.discharges != null) {
+            val (min, max) = this.discharges
+            rangeSpecification(min, max, CostRecord_.totalDischarges)?.and(it) ?: it
+        } else {
+            it
+        }
+    }.letIf(this.averageCoveredCharges != null) {
+        val (min, max) = this.averageCoveredCharges!!
+        rangeSpecification(min, max, CostRecord_.averageCoveredCharges)?.and(it) ?: it
+    }.letIf(this.averageMedicarePayments != null) {
+        val (min, max) = this.averageMedicarePayments!!
+        rangeSpecification(min, max, CostRecord_.averageMedicarePayments)?.and(it) ?: it
+    }.letIf(this.state != null) {
+        Specification<CostRecord> { record, _, criteriaBuilder ->
             val join: Join<CostRecord, MedicalProvider> = record.join(CostRecord_.medicalProvider)
             criteriaBuilder.equal(join.get<String>(MedicalProvider_.state), state)
-        })
+        }.and(it)
     }
-    return specifications.reduce { a, b -> a.and(b) }
-
 }
 
-private inline fun <reified T : Number> rangeSpecification(min: T?, max: T?, specifications: MutableList<Specification<CostRecord>>, field: SingularAttribute<CostRecord, T>?) {
-    when {
+private inline fun <reified T> T.letIf(check: Boolean, operation: (T) -> T) = if (check) {
+    this.let(operation)
+} else {
+    this
+}
+
+
+private inline fun <reified T : Number> rangeSpecification(min: T?, max: T?, field: SingularAttribute<CostRecord, T>?): Specification<CostRecord>? {
+    return when {
         // Could not use `between` because the reification does not play well with it.
-        min != null && max != null -> specifications.add(Specification { record, _, criteriaBuilder -> criteriaBuilder.and(criteriaBuilder.ge(record.get(field), min), criteriaBuilder.le(record.get(field), max)) })
-        min != null -> specifications.add(Specification { record, _, criteriaBuilder -> criteriaBuilder.ge(record.get(field), min) })
-        max != null -> specifications.add(Specification { record, _, criteriaBuilder -> criteriaBuilder.le(record.get(field), max) })
+        min != null && max != null -> Specification { record, _, criteriaBuilder -> criteriaBuilder.and(criteriaBuilder.ge(record.get(field), min), criteriaBuilder.le(record.get(field), max)) }
+        min != null -> Specification { record, _, criteriaBuilder -> criteriaBuilder.ge(record.get(field), min) }
+        max != null -> Specification { record, _, criteriaBuilder -> criteriaBuilder.le(record.get(field), max) }
+        else -> null
     }
 }
